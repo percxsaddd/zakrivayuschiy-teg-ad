bdd6cae4092b3042b65d6e9e5e5a9775ed7ae627
percxsaddd <p4kostb@yandex.ru>
2025-12-14 22:22:49 +0400

    w

--- scripts/like.js
+++ scripts/like.js
@@ -3,12 +3,17 @@ const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.onclick = (event) => {
+    event.preventDefault();
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  };
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+  button.onclick = (event) => {
+    event.preventDefault();
+    toggleIsLiked(likeHeartArray[index], button);
+  };
 });
 
 function toggleIsLiked(heart, button) {
